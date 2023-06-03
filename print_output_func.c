70016066c55a74ef5218ee3c79e926e54738f08e
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-06-03 05:10:15 +0100

    Update print_output_func.c

--- print_output_func.c
+++ print_output_func.c
@@ -14,7 +14,7 @@ int _putchar (char c)
         static char buf[1024];
         static int i;
 
-        if (c == -1 || i >= 1024)
+        if (c == -1 || i == 1024)
         {
                 write(1, &buf, i);
                 i = 0;
