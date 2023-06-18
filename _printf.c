453f749d59d399760ae55d1525396e3455eda235
maryagiamah <maryagiamah@gmail.com>
2023-06-18 18:29:32 +0100

    trying to debug

--- _printf.c
+++ _printf.c
@@ -5,58 +5,6 @@
 
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-    int (*print_func)(va_list l, flags_t *f, len_mod *m, int fld_wdth, int prec_sn);
-    const char *fmt_str;
-    va_list var_args;
-    int count = 0, fld_wdth = 0, prec_sn = 1;
-    flags_t flags;
-    len_mod mod_f;
-    va_start(var_args, format);
-
-
-    if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
-    for (fmt_str = format; *fmt_str; fmt_str++)
-    {
-        initialize_variables(&flags, &mod_f);
-        fld_wdth = 0, prec_sn = 1;
-        if (*fmt_str == '%')
-        {
-            fmt_str++;
-            if (*fmt_str == '%')
-            {
-                count += _putchar('%');
-                continue;
-            }
-	    if (*fmt_str == 'n')
-            {
-                print_n(var_args, count);
-                continue;
-            }
-            while (get_flag(*fmt_str, &flags))
-                fmt_str++;
-            if (is_digit(*fmt_str))
-            {
-                fld_wdth = ((*fmt_str) - '0');
-                fmt_str++;
-            }
-            else if (*fmt_str == '*')
-            {
-                fld_wdth = va_arg(var_args, int);
-                fmt_str++;
-            }
-          if (fld_wdth < 0)
-          {
-               fld_wdth *= -1;
-		  flags.dash_flag = 1;
-	  }
-            if (*fmt_str == '.')
-            {
-                fmt_str++;
-=======
 	int (*print_func)(va_list l, flags_t *f, len_mod *m, int fld_wdth, int prec_sn);
         const char *fmt_str;
 	va_list var_args;
@@ -108,7 +56,6 @@ int _printf(const char *format, ...)
             	if (*fmt_str == '.')
             	{
                 	fmt_str++;
->>>>>>> 0b3e73c (trying to debug)
                 if (is_digit(*fmt_str))
                 {
 
