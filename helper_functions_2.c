03346b6f27b5acc79de24017e46d4e34f2dca9b0
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-06-19 23:14:00 +0100

    Update helper_functions_2.c

--- helper_functions_2.c
+++ helper_functions_2.c
@@ -38,9 +38,7 @@ int apply_width_precision_2 (long int n, flags_t *f, int fld_wdth, int prec_sn, 
         int dig_len = count_signed_digits(n);
         int count = 0;
         int padding = ' ';
-
-	if (n < 0)
-		_putchar('-');
+	
 	if (prec_sn > 1)
 		f->zero_flag = 0;
 	else if (prec_sn == 0)
