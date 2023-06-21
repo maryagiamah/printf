f846f115e682d91da572f15a0ac39d7817fc9b0d
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-06-21 14:55:34 +0100

    Update print_char_strings.c

--- print_char_strings.c
+++ print_char_strings.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <string.h>
-#include <stddef.h>
 /**
  * print_string - prints a string
  * @l: va_list arguments from _printf
@@ -28,7 +27,7 @@ int print_string (va_list l, flags_t *f, len_mod *m, int fld_wdth, int prec_sn)
 		count = zero_precision(count, fld_wdth, f);
 		return count;
 	}
-	if (pre_csn > 1 && prec_sn < count)
+	if (prec_sn > 1 && prec_sn < count)
         {
 		
 		s[prec_sn] = '\0';
