48f9e717d26e1bd79776f74e42ac480d18b5ce01
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-07-07 01:33:25 +0100

    Update print_char_strings.c

--- print_char_strings.c
+++ print_char_strings.c
@@ -17,8 +17,7 @@ int print_string (va_list l, flags_t *f, len_mod *m, int fld_wdth, int prec_sn)
         (void)m;
 	if (!s)
 	{
-                *s = "(null) ";
-		return(_puts(s));
+		return(_puts("(null) "));
 	}
 	if (prec_sn == 0)
 	{
