d95ec1b463dba4f1ac42eb67313c3d1ba6fc9f41
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-08-13 23:22:58 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -68,7 +68,7 @@ int _printf(const char *format, ...)
 		    	fmt_str++;
                 }
 		else
-		   	prec_sn = 1;
+		   	prec_sn = 0;
             	}
           	while (get_mod(*fmt_str, &mod_f))
 	  	{
